b494439b0cf68e15bacdef6de76ae24d2eaa9a03
Satyaki Bhattacharjee <satyakit1998@gmail.com>
2025-01-07 10:19:41 +0000

    add new two feature project

---
+++ react_practice/src/App.tsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+  
+  const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Mango",
+    "Pineapple",
+    "Grapes",
+    "Strawberry",
+    "Blueberry",
+    "Peach",
+    "Cherry",
+    "Watermelon",
+    "Papaya",
+    "Kiwi",
+    "Pomegranate",
+    "Lemon",
+    "Lychee",
+    "Coconut",
+    "Dragon Fruit",
+    "Raspberry",
+    "Blackberry"
+  ];
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
