f72546111952b066229d89f429e1054e2972f330
Satyaki Bhattacharjee <140399051+SatyakiB7076@users.noreply.github.com>
2025-02-04 18:00:54 +0000

    formatting

--- search-feature-react/src/Components/Search.jsx
+++ search-feature-react/src/Components/Search.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
-const Search = ({handleSearch}) => {
-    const [item,setSearchItem]=useState("");
-    const handleSearchItem=(data)=>{
-setSearchItem(data);
-handleSearch(item);
-    }
+const Search = ({ handleSearch }) => {
+  const [item, setSearchItem] = useState("");
+  const handleSearchItem = (data) => {
+    setSearchItem(data);
+    handleSearch(item);
+  }
   return (
     <div className='m-5'>
-      <input type="search" name="search" id="search" placeholder='Search a movie' onChange={(e)=>handleSearchItem(e.target.value)}/>
+      <input type="search" name="search" id="search" placeholder='Search a movie' onChange={(e) => handleSearchItem(e.target.value)} />
     </div>
   )
 }
