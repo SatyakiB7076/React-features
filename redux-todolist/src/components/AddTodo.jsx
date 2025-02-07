4a79a76d9c64ede277317ef850318c42dcf28df5
Satyaki Bhattacharjee <140399051+SatyakiB7076@users.noreply.github.com>
2025-02-07 15:58:25 +0000

    add frontend to add to do

---
+++ redux-todolist/src/components/AddTodo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+
+const AddTodo = () => {
+
+const dispatch=useDispatch();
+
+    const {input,setInput}=useState("");
+    const addTodoHandler=()=>{
+
+    }
+  return (
+    <div>
+      <form onSubmit={addTodoHandler} className="flex gap-2 items-center bg-white p-4 rounded-lg shadow-md">
+      <input
+        type="text"
+        className="flex-1 p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+        placeholder="Add new todo here..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition"
+      >
+        Add Todo
+      </button>
+    </form>
+    </div>
+  )
+}
+
+export default AddTodo
--- redux-todolist/src/features/todo/todoSlice.js
+++ redux-todolist/src/features/todo/todoSlice.js
@@ -9,7 +9,30 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state,action) => { },
-        removeTodo: (state,action) => { },
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo);
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) =>
+                todo.id !== action.payload
+            )
+        },
+        updateTodo: (state, action) => {
+            //first find the todo using id which needed to update
+            const todo = state.todos = state.todos.find((todo) =>
+                todo.id === action.payload);
+            //if todo exists then change the text field from the payload
+            if (todo) {
+                todo.text = action.payload.text;
+            }
+        }
     }
 })
+//one export for individual use cases in the component
+export const {addTodo,removeTodo,updateTodo}=todoSlice.actions;
+//one export for the store
+export default todoSlice.reducer;
