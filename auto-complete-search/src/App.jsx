f04171ae128dca229569226312d9e3c5a732ed49
Satyaki <140399051+SatyakiB7076@users.noreply.github.com>
2025-12-06 05:23:39 +0000

    add new files

---
+++ auto-complete-search/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3>Auto complete Search bar</h3>
+      <input type="search" placeholder="Search here...." />
+    </>
+  );
+}
+
+export default App;
