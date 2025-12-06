5ded797ce760bd62ddecd77d719d624aae5631d3
SatyakiB7076 <satyakit1998@gmail.com>
2025-12-06 11:12:20 +0530

    add new files

---
+++ autocomplete-search/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Auto complete Search Bar</h2>
+      <input type="search" placeholder='Search Here...' />
+    </>
+  )
+}
+
+export default App
